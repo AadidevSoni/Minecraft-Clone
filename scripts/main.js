fa8fb9d59ed0a7ce29c56e97a74ef9822b1e6ca1
Aadidev Soni <aadidevbahrain@gmail.com>
2025-08-08 21:18:25 +0530

    World and control setup

--- scripts/main.js
+++ scripts/main.js
@@ -9,18 +9,18 @@ document.body.appendChild(renderer.domElement);
 
 //Camera
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,2000);
-camera.position.set(2,2,2);
-camera.lookAt(0,0,0);
+camera.position.set(-32,16,-32);
+//camera.lookAt(0,0,0);
 
 //Controls
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(16,0,16); //Middle of world
+controls.update();
 
 //Scene
 const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({color: 0x00d000});
-const cube = new THREE.Mesh(geometry,material);
-scene.add(cube);
 
 //Lighting
 function setupLights() {
@@ -37,11 +37,24 @@ function setupLights() {
   scene.add(ambient);
 }
 
+//World Setup
+function setupWorld(size) {
+  for(let x=0;x<size;x++) {
+    for(let z=0;z<size;z++) {
+      const cube = new THREE.Mesh(geometry,material);
+      cube.position.set(x,0,z);
+      scene.add(cube);
+    }
+  }
+}
+
 //Render loop
 function animate() {
   requestAnimationFrame(animate);
+  /*
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  */
   renderer.render(scene,camera);
 }
 
@@ -53,4 +66,5 @@ window.addEventListener('resize', () => {
 })
 
 setupLights();
+setupWorld(32);
 animate();
