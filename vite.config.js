fcb8459e5cc3bfd7bbe39a26db5e1945affa7799
Aadidev Soni <aadidevbahrain@gmail.com>
2025-08-08 20:34:55 +0530

    Initial Setup

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+/**
+* @type {import('vite').UserConfig}
+*/
+//lets us see the file and line where the error is appearing using sourcemap
+export default {
+  base: '/minecraft-threejs-clone/',
+  build: {
+    sourcemap: true
+  }
+}
