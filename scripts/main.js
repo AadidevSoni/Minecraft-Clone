e3d3376dc5ec809b7f91aadd1ae56b2cc1d8aa98
Aadidev Soni <aadidevbahrain@gmail.com>
2025-08-08 21:25:11 +0530

    World Generation Modified

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import {World} from "./world";
 
 //Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setClearColor(0x80a0e0);
 document.body.appendChild(renderer.domElement);
 
 //Camera
@@ -19,8 +21,9 @@ controls.update();
 
 //Scene
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshLambertMaterial({color: 0x00d000});
+const world = new World();
+world.generate();
+scene.add(world);
 
 //Lighting
 function setupLights() {
@@ -35,17 +38,6 @@ function setupLights() {
   const ambient = new THREE.AmbientLight();
   ambient.intensity = 0.1;
   scene.add(ambient);
-}
-
-//World Setup
-function setupWorld(size) {
-  for(let x=0;x<size;x++) {
-    for(let z=0;z<size;z++) {
-      const cube = new THREE.Mesh(geometry,material);
-      cube.position.set(x,0,z);
-      scene.add(cube);
-    }
-  }
 }
 
 //Render loop
@@ -66,5 +58,4 @@ window.addEventListener('resize', () => {
 })
 
 setupLights();
-setupWorld(32);
 animate();
